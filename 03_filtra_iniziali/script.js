5d85595eab5f9498c9978b842ee2aabc424830e3
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-17 16:14:11 +0100

    fix third snack

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,27 +1,24 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+let letter = "A";
 
 // Dichiara la funzione qui.
-function letterA(names) {
-    let letter = ["A"];
+function letterA(names, letter) {
+    let newArray = [];
     for (let i = 0; i < names.length; i++) {
-        first = names[i];
+        name = names[i];
 
-        if(letter[0] === first[0]) {
-            letter.push(first);
-            return letter;
-        }
-        else {
-            return false;
+        if(letter[0] === name[0]) {
+            newArray.push(name);
         }
     }
+    return newArray;
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
-const isLetterA = letterA(names);
+const isLetterA = letterA(names, letter);
 console.log(isLetterA);
 
 
