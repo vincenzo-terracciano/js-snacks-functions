c867f541f1ed9bedaff1435f195383a677a19f09
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-17 17:41:55 +0100

    fix and add arrow function in the fourth snack

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,20 +3,34 @@
 const word = 'javascript';
 
 // Dichiara la funzione qui.
-function howManyVocal (word) {
+
+/* function declaration */
+/* function howManyVocal (word) {
     let vocal = 0;
     for( let i = 0; i < word.length; i++) {
         character = word[i];
         if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
             vocal++
         }
-        return vocal
     }
+    return vocal
+} */
+
+/* aroow function */
+const howManyVocal = word => {
+    let vocal = 0;
+    for( let i = 0; i < word.length; i++) {
+        character = word[i];
+        if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
+            vocal++
+        }
+    }
+    return vocal
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 const numberVocal = howManyVocal(word);
-console.log(numberVocal);
+console.log(`The word ${word} has ${numberVocal} vocals (a, a, i)`);
 
 
 
