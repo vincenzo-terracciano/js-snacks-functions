7913511dc96b86c3f7f12725b706393ff8a53cf7
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-17 15:55:50 +0100

    add third snack

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function letterA(names) {
+    let letter = ["A"];
+    for (let i = 0; i < names.length; i++) {
+        first = names[i];
+
+        if(letter[0] === first[0]) {
+            letter.push(first);
+            return letter;
+        }
+        else {
+            return false;
+        }
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const isLetterA = letterA(names);
+console.log(isLetterA);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
